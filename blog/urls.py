d718becbaaad9745bdafc220c45929531daa8997
adriana <adriana.quijada@hotmail.com>
2015-11-07 17:44:59 -0700

    Cambie el HTML de la pagina

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
+from django.conf.urls import include, url
+from . import views
+urlpatterns = [ 
+	url(r'^$', views.post_list),
+]
 from django.shortcuts import render
-
 # Create your views here.
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
+
